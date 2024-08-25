85ba63cf377c80e3f55c31de67af09937f26f0f2
osamaasgharsulfi <asgharusama4@gmail.com>
2024-08-25 19:17:22 +0500

    added helath check code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,33 +1,14 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
-import { SchoolService } from './school/school.service';
-import { School } from './school/entities/school.entity';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('schools')
-export class SchoolController {
-  constructor(private readonly schoolService: SchoolService) {}
-
-  @Post()
-  async createSchool(@Body() schoolData: any) {  // Using 'any' instead of DTO
-    return await this.schoolService.createOrUpdateSchool(schoolData);
-  }
-
-  @Put(':id')
-  async updateSchool(@Param('id') id: number, @Body() schoolData: any) {  // Using 'any' instead of DTO
-    return await this.schoolService.createOrUpdateSchool(schoolData, id);
-  }
-
-  @Get(':id')
-  async getSchoolById(@Param('id') id: number) {
-    return await this.schoolService.getSchoolById(id);
-  }
-
+@Controller()
+export class AppController {
   @Get()
-  async getAllSchools() {
-    return await this.schoolService.getAllSchools();
+  getWelcomeMessage(): string {
+    return 'Welcome to the School Management System API!';
   }
 
-  @Delete(':id')
-  async deleteSchool(@Param('id') id: number) {
-    return await this.schoolService.deleteSchool(id);
+  @Get('health')
+  getHealthStatus(): string {
+    return 'The application is up and running!';
   }
 }
